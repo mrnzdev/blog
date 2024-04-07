31a616ea31df2d105d0995bc6150c4ac6a3db678
mrnzdev <contact@mrnz.dev>
2024-04-07 00:08:52 -0300

    Added cloudflare for the SSR in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import db from '@astrojs/db';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), db()],
-	prefetch: {
-		prefetchAll: true
-	}
+  integrations: [tailwind(), db()],
+  prefetch: {
+    prefetchAll: true
+  },
+  output: "server",
+  adapter: cloudflare()
 });

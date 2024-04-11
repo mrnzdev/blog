06922513bbeca189dff86bdaa36baa07688deb2e
mrnzdev <contact@mrnz.dev>
2024-04-11 11:41:52 -0300

    Removed cloudflare integration, as it was not working properly and was causing issues with the deploy.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
-import cloudflare from '@astrojs/cloudflare';
 import db from '@astrojs/db';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), db()],
-	prefetch: {
-		prefetchAll: true
-	},
-	output: 'server',
-	adapter: cloudflare()
+	integrations: [tailwind(), db()]
 });

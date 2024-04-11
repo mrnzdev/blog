71ed8b70ce416d5c746f8beea7c599145c2c9245
mrnzdev <contact@mrnz.dev>
2024-04-11 11:37:38 -0300

    Sorted the imports in the astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 import db from '@astrojs/db';
-
-import cloudflare from "@astrojs/cloudflare";
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()],
-  prefetch: {
-    prefetchAll: true
-  },
-  output: "server",
-  adapter: cloudflare()
+	integrations: [tailwind(), db()],
+	prefetch: {
+		prefetchAll: true
+	},
+	output: 'server',
+	adapter: cloudflare()
 });
